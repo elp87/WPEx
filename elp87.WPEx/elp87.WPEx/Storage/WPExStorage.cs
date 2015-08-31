3bd4661ee799e3f33689ec37911645bf3201e7ea
elp87 <elp1987@gmail.com>
2015-08-31 12:30:06 +0300

    WPExStorage: Add IsFileExists method

---
+++ elp87.WPEx/elp87.WPEx/Storage/WPExStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace elp87.WPEx.Storage
+{
+    public static class WPExStorage
+    {
+        public static async Task<bool> IsFileExists(this StorageFolder folder, string fileName)
+        {
+            try { StorageFile file = await folder.GetFileAsync(fileName); }
+            catch { return false; }
+            return true;
+        }
+    }
+}
